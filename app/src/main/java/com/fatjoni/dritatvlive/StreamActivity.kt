d27262c7ae737aa993d95739c709f591ea77a800
Fatjon Hushi <me@fatjoni.com>
2022-07-22 14:27:48 +0200

    initial commit

---
+++ app/src/main/java/com/fatjoni/dritatvlive/StreamActivity.kt
@@ -0,0 +1,101 @@
+package com.fatjoni.dritatvlive
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.media3.common.MediaItem
+import androidx.media3.common.MimeTypes
+import androidx.media3.common.util.Util
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.trackselection.DefaultTrackSelector
+import com.fatjoni.dritatvlive.databinding.ActivityStreamBinding
+
+class StreamActivity : AppCompatActivity() {
+    private val viewBinding by lazy(LazyThreadSafetyMode.NONE) {
+        ActivityStreamBinding.inflate(layoutInflater)
+    }
+    private var player: ExoPlayer? = null
+    private var playWhenReady = true
+    private var currentItem = 0
+    private var playbackPosition = 0L
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(viewBinding.root)
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        if (Util.SDK_INT > 23) {
+            initializePlayer()
+        }
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        hideSystemUi()
+        if ((Util.SDK_INT <= 23 || player == null)) {
+            initializePlayer()
+        }
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        if (Util.SDK_INT <= 23) {
+            releasePlayer()
+        }
+    }
+
+
+    public override fun onStop() {
+        super.onStop()
+        if (Util.SDK_INT > 23) {
+            releasePlayer()
+        }
+    }
+
+    @SuppressLint("InlinedApi")
+    private fun hideSystemUi() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, viewBinding.videoView).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+    }
+
+    private fun initializePlayer() {
+        val trackSelector = DefaultTrackSelector(this).apply {
+            setParameters(buildUponParameters().setMaxVideoSizeSd())
+        }
+        player = ExoPlayer.Builder(this)
+            .setTrackSelector(trackSelector)
+            .build()
+            .also { exoPlayer ->
+                viewBinding.videoView.player = exoPlayer
+                val mediaItem = MediaItem.Builder()
+                    .setUri(getString(R.string.media_url_hls))
+                    .setMimeType(MimeTypes.APPLICATION_M3U8)
+                    .build()
+
+                exoPlayer.setMediaItem(mediaItem)
+
+                exoPlayer.playWhenReady = playWhenReady
+                //exoPlayer.seekTo(currentItem, playbackPosition)
+                exoPlayer.prepare()
+            }
+
+    }
+
+    private fun releasePlayer() {
+        player?.let { exoPlayer ->
+            playbackPosition = exoPlayer.currentPosition
+            currentItem = exoPlayer.currentMediaItemIndex
+            playWhenReady = exoPlayer.playWhenReady
+            exoPlayer.release()
+        }
+        player = null
+    }
+}
