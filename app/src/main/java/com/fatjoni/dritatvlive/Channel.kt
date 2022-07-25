4558fd9315106b5184f6306cbc897681659dc00a
Fatjan Hushi <me@fatjoni.com>
2022-07-25 22:39:18 +0200

    add firebase UI

---
+++ app/src/main/java/com/fatjoni/dritatvlive/Channel.kt
@@ -0,0 +1,6 @@
+package com.fatjoni.dritatvlive
+
+data class Channel(val name: String="Drita TV",
+                   val link: String = "https://monamour.agency/dritaweb008/index.m3u8"){
+
+}
--- app/src/main/java/com/fatjoni/dritatvlive/FirstFragment.kt
+++ app/src/main/java/com/fatjoni/dritatvlive/FirstFragment.kt
@@ -2,12 +2,15 @@ package com.fatjoni.dritatvlive
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
 import com.fatjoni.dritatvlive.databinding.FragmentFirstBinding
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.Query
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -38,6 +41,17 @@ class FirstFragment : Fragment() {
             val intent = Intent(context, StreamActivity::class.java)
             startActivity(intent)
         }
+        // Write a message to the database
+        //val database = Firebase.database("https://drita-media-default-rtdb.europe-west1.firebasedatabase.app/")
+        //Firebase.database.setPersistenceEnabled(true)
+        //val myRef = database.getReference("message")
+
+        //myRef.setValue("Hello, World!")
+
+        val query: Query = Firebase.database("https://drita-media-default-rtdb.europe-west1.firebasedatabase.app/")
+            .reference
+            .child("channels")
+            .limitToLast(10)
     }
 
     override fun onDestroyView() {
